ae3fdc26fbb0ce3d30d7bdc655b1b79458639259
Uko Uwatt <uwattuko@gmail.com>
2024-07-11 13:36:25 +0100

    task 2

---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,7 @@
+-- create script that ranks country origin of bands
+-- ordered by number of (non-unique) fans
+
+SELECT origin, SUM(fans) as nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
