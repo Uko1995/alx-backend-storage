e683a70cda550a3fff9858f0ddec8b28c7ff0aee
Uko Uwatt <uwattuko@gmail.com>
2024-07-11 14:01:26 +0100

    task 3

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,7 @@
+-- create script that lists all bands with 'Glam rock' as thier main style
+-- by longevity
+
+SELECT band_name, (IF(split IS NULL, 2022, split) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
