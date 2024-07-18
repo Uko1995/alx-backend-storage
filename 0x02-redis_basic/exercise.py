89e4f7021022c64d9cbe51d62f0d9f250549e932
Uko Uwatt <uwattuko@gmail.com>
2024-07-18 04:30:04 +0100

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,14 +2,14 @@
 ''' create cache class and do some operations on it'''
 import redis
 import uuid
-from typing import Union
+from typing import Union, Any, Callable
 
 
 class Cache:
     '''cache class'''
 
-    def __init__(self):
-        '''init method'''
+    def __init__(self) -> None:
+        '''__init__ method'''
         self. _redis = redis.Redis()
         self. _redis.flushdb()
 
@@ -18,3 +18,20 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union[
+            str, bytes, int, float]:
+        '''get method'''
+        data = self._redis.get(key)
+        if fn is None:
+            return data
+        return fn(data)
+
+    def get_int(self, key: str) -> int:
+        '''get_int method'''
+        return int(self._redis.get(key)) if self._redis.get(key) else 0
+
+    def get_int(self, key: str) -> str:
+        '''get_str method'''
+        data = self._redis.get(key)
+        return data.decode('UTF-8') if data else ''
